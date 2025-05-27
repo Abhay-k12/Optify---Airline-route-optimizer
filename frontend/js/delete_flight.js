706b20cebcaea7f43029f6caa749ab368212446f
anvesha23rawat <151908105+anvesha23rawat@users.noreply.github.com>
2025-05-27 15:41:55 +0530

    Delete frontend directory

--- frontend/js/delete_flight.js
+++
@@ -1,7 +0,0 @@
-function deleteFlight(event) {
-  event.preventDefault();
-  const flightId = document.getElementById('flightId').value;
-  // Placeholder for backend integration
-  document.getElementById('output').innerText = `Flight Deleted:\nID: ${flightId}`;
-  document.getElementById('deleteFlightForm').reset();
-}
